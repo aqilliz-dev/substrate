2cc95f25a5d9e215c034c2f1d9840b1953089239
Nacho Pal <ig.pal.san@gmail.com>
2020-12-14 22:29:12 +0800

    wip reconciled data algorithm

--- bin/node-template/pallets/data-reconciliation/src/lib.rs
+++ bin/node-template/pallets/data-reconciliation/src/lib.rs
@@ -5,12 +5,14 @@
 
 use frame_support::{
 	debug,
+	ensure,
 	weights::{Weight, Pays},
-    decl_module, decl_event, decl_storage,
+    decl_module, decl_event, decl_storage, decl_error,
 	storage::{StorageDoubleMap, StorageMap, StorageValue},
 	codec::{Encode, Decode},
-	sp_runtime::RuntimeDebug
+	sp_runtime::{RuntimeDebug, Percent, FixedU128}
 };
+
 use frame_system::{self as system, ensure_signed};
 
 use sp_core::Hasher;
@@ -29,6 +31,8 @@ pub trait Trait: system::Trait {
 	// type WeightInfo: WeightInfo;
 }
 
+const QUINTILLION: u128 = 1_000_000_000_000_000_000;
+
 pub type CampaignId = Vec<u8>;
 pub type Platform = Vec<u8>;
 pub type Source = Vec<u8>;
@@ -38,18 +42,18 @@ pub type DateCampaign = Vec<u8>;
 #[derive(Encode, Decode, Clone, Default, RuntimeDebug, PartialEq, Eq)]
 pub struct Campaign {
     name: Vec<u8>,
-	total_budget: u64,
+	total_budget: u128,
 	currency: Vec<u8>,
 	start_date: Date,
 	end_date: Date,
 	platforms: Vec::<Platform>,
 	advertiser: Vec<u8>,
 	brand: Vec<u8>,
-	reconciliation_threshold: u64,
+	reconciliation_threshold: u128,
 	version: u8,
-	cpc: (bool, u64),
-	cpm: (bool, u64),
-	cpl: (bool, u64),
+	cpc: (bool, u128),
+	cpm: (bool, u128),
+	cpl: (bool, u128),
 }
 
 #[derive(Encode, Decode, Clone, Default, RuntimeDebug, PartialEq, Eq)]
@@ -58,22 +62,24 @@ pub struct AggregatedData {
 	platform: Platform,
 	date: Date,
 	source: Source,
-	impressions: u64,
-	clicks: u64,
-	conversions: u64,
+	impressions: u128,
+	clicks: u128,
+	conversions: u128,
 }
 
 #[derive(Encode, Decode, Clone, Default, RuntimeDebug, PartialEq, Eq)]
 pub struct Kpis {
-	sources: Vec::<(Platform, u64)>,
-	final_count: u64,
-	cost: u64,
+	final_count: u128,
+	cost: u128,
+	zdmp: u128,
+	platform: u128,
+	client: u128
 }
 
 #[derive(Encode, Decode, Clone, Default, RuntimeDebug, PartialEq, Eq)]
 pub struct ReconciledData {
-	amount_spent: u64,
-	budget_utilisation: u64,
+	amount_spent: u128,
+	budget_utilisation: u128,
 	clicks: Kpis,
 	impressions: Kpis,
 	conversions: Kpis,
@@ -111,10 +117,21 @@ decl_event! {
     }
 }
 
+decl_error! {
+	pub enum Error for Module<T: Trait> {
+		/// Campaign ID does not exist
+		CampaignDoesNotExist,
+
+		// /// The value cannot be incremented further because it has reached the maimum allowed value
+		// MaxValueReached,
+	}
+}
+
 
 decl_module! {
 	/// The module declaration.
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+		type Error = Error<T>;
 
 		#[weight = (0, Pays::No)]
 		fn set_campaign(origin, campaign_id: CampaignId, campaign: Campaign) {
@@ -137,6 +154,8 @@ decl_module! {
 		#[weight = (0, Pays::No)]
 		fn set_aggregated_data(origin, aggregated_data: AggregatedData) {
 			let sender = ensure_signed(origin)?;
+
+			// ensure!(<Campaigns>::contains_key(&aggregated_data.campaign_id), Error::<T>::CampaignDoesNotExist);
 
 			<ReconciledDateCampaigns>::insert(&aggregated_data.date, &aggregated_data.campaign_id, true);
 			<ReconciledCampaignDates>::insert(&aggregated_data.campaign_id, &aggregated_data.date, true);
@@ -166,34 +185,122 @@ impl<T: Trait> Module<T> {
 		debug::info!("Date-Campaign {:?}", *date_campaign);
 		debug::info!("Aggregated data {:?}", *aggregated_data);
 
-		// let reconciled_campaign_dates_1 = <ReconciledCampaignDates>::get(&aggregated_data.campaign_id, &aggregated_data.date);
-		// let campaign_date_exists = <ReconciledData>::contains_key(&campaign_date);
-		// let reconciled_campaign_dates_2 = <ReconciledCampaignDates>::get(&aggregated_data.campaign_id, b"-".to_vec());
-		// let reconciled_campaign_dates_3 = <ReconciledCampaignDates>::get(b"-".to_vec(), b"-".to_vec());
+		let mut sources: Vec::<(Platform, u64)> = Vec::new();
 
-		// debug::info!("Reconciled data 1 {:?}", reconciled_campaign_dates_1);
-		// debug::info!("Reconciled data 1 {:?}", reconciled_campaign_dates_1);
-		// debug::info!("Reconciled data 2 {:?}", reconciled_campaign_dates_2);
-		// debug::info!("Reconciled data 3 {:?}", reconciled_campaign_dates_3);
-		// let source
-		// let impressions = Kpis {
+		let campaign_date_platform_exists = <ReconciledDataStore>::contains_key(&date_campaign, &aggregated_data.platform);
 
-		// };
+		if campaign_date_platform_exists == false {
+			Self::create_recociled_data_record(&date_campaign, &aggregated_data);
+		} else {
+			Self::update_recociled_data_record(&date_campaign, &aggregated_data)
+		}
+	}
 
-		// let clicks = Kpis {
+	fn create_recociled_data_record(date_campaign: &DateCampaign, aggregated_data: &AggregatedData) {
+		let mut kpis_impressions = Kpis {
+			final_count: 0,
+			cost: 0,
+			zdmp: 0,
+			platform: 0,
+			client: 0
+		};
 
-		// };
+		let mut kpis_clicks = Kpis {
+			final_count: 0,
+			cost: 0,
+			zdmp: 0,
+			platform: 0,
+			client: 0
+		};
 
-		// let conversions = Kpis {
+		let mut kpis_conversions = Kpis {
+			final_count: 0,
+			cost: 0,
+			zdmp: 0,
+			platform: 0,
+			client: 0
+		};
 
-		// };
+		Self::update_kpis(&aggregated_data, &mut kpis_impressions, &mut kpis_clicks, &mut kpis_conversions);
 
-		// let data = ReconciledData {
+		kpis_impressions.final_count = *(&aggregated_data.impressions);
+		kpis_clicks.final_count = *(&aggregated_data.clicks);
+		kpis_conversions.final_count = *(&aggregated_data.conversions);
+
+		let record = ReconciledData {
+			amount_spent: 0,
+			budget_utilisation: 0,
+			impressions: kpis_impressions,
+			clicks: kpis_clicks,
+			conversions: kpis_conversions
+		};
+
+		<ReconciledDataStore>::insert(&date_campaign, &aggregated_data.platform, record);
+	}
+
+	fn update_recociled_data_record(date_campaign: &DateCampaign, aggregated_data: &AggregatedData) {
+		let mut record = <ReconciledDataStore>::get(&date_campaign, &aggregated_data.platform);
+
+		// let mut kpis_impressions = record.impressions;
+		// let mut kpis_clicks = record.clicks;
+		// let mut kpis_conversions = record.conversions;
+
+		// Self::update_kpis(&aggregated_data, &mut kpis_impressions, &mut kpis_clicks, &mut kpis_conversions);
+		Self::update_kpis(&aggregated_data, &mut record.impressions, &mut record.clicks, &mut record.conversions);
+
+		Self::run_reconciliation(&aggregated_data, &mut record);
+
+		<ReconciledDataStore>::insert(&date_campaign, &aggregated_data.platform, record);
+	}
+
+	fn update_kpis(
+		aggregated_data: &AggregatedData,
+		kpis_impressions: &mut Kpis,
+		kpis_clicks: &mut Kpis,
+		kpis_conversions: &mut Kpis
+	) {
+		if *(&aggregated_data.source) == b"zdmp".to_vec() {
+			kpis_impressions.zdmp = aggregated_data.impressions;
+			kpis_clicks.zdmp = aggregated_data.clicks;
+			kpis_conversions.zdmp = aggregated_data.conversions;
+		} else if *(&aggregated_data.source) == b"client".to_vec() {
+			kpis_impressions.client = aggregated_data.impressions;
+			kpis_clicks.client = aggregated_data.clicks;
+			kpis_conversions.client = aggregated_data.conversions;
+		} else {
+			kpis_impressions.platform = aggregated_data.impressions;
+			kpis_clicks.platform = aggregated_data.clicks;
+			kpis_conversions.platform = aggregated_data.conversions;
+		}
+	}
+
+	fn run_reconciliation(aggregated_data: &AggregatedData, record: &mut ReconciledData) {
+
+		let campaign = <Campaigns>::get(&aggregated_data.campaign_id);
+		let reconciliation_threshold = campaign.reconciliation_threshold;
+
+		let impressions: FixedU128 = FixedU128::from_inner(*(&record.impressions.client)* QUINTILLION);
+		// let impressions: Percent = Percent::from_percent(*(&aggregated_data.impressions));
+		// let percent_impressions: Percent = Percent::from_percent(reconciliation_threshold);
+		let percent_impressions: FixedU128 = FixedU128::from_inner(reconciliation_threshold) / FixedU128::from_inner(100);
+
+		// impressions * percent_impressions;
+
+
+		debug::info!("Threshold {:?}", percent_impressions);
+		debug::info!("Impresions {:?}", *(&record.impressions.client));
+		debug::info!("Impresions U128 {:?}", impressions);
+		debug::info!("Result U128 {:?}", impressions * percent_impressions);
+
+		// kpis_clicks.final_count = *(&aggregated_data.clicks);
+		// kpis_conversions.final_count = *(&aggregated_data.conversions);
+		// *(&record).
+		// let record = ReconciledData {
 		// 	amount_spent: 0,
 		// 	budget_utilisation: 0,
-		// 	impressions: impressions,
-		// 	clicks: clicks,
-		// 	conversions: conversions
+		// 	impressions: *(&kpis_impressions),
+		// 	clicks: *kpis_clicks,
+		// 	conversions: *kpis_conversions
 		// };
 	}
 }
