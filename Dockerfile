12e9ea03e2067cdd5f5b8b091d027d57a600e8e2
Siva <siva@aqilliz.com>
2023-10-19 00:10:20 +0530

    V4 substrate upgrade

--- Dockerfile
+++ Dockerfile
@@ -1,51 +1,70 @@
-FROM ubuntu:18.04 as substrate
+FROM rust:1.73.0-buster as substrate
 
 WORKDIR /substrate
 
 EXPOSE 9944 30333 9933
 
-RUN apt-get update \
-	&& apt upgrade -y \
-	&& apt install -y curl \
-	&& curl https://getsubstrate.io -sSf | bash -s -- --fast
+RUN apt-get update && apt upgrade -y 
+RUN apt install build-essential -y
+RUN apt install --assume-yes git clang curl libssl-dev protobuf-compiler
+RUN rustup default stable
+RUN rustup update
+RUN rustup update nightly
+RUN rustup target add wasm32-unknown-unknown --toolchain nightly
 
-RUN curl https://sh.rustup.rs -sSf -y | sh
+# RUN apt-get update \
+# 	&& apt upgrade -y \
+# 	&& apt install -y curl \
+# 	&& curl https://getsubstrate.io -sSf | bash -s -- --fast
 
-ENV PATH="/root/.cargo/bin:${PATH}"
+# RUN curl https://sh.rustup.rs -sSf -y | sh
 
-RUN rustup toolchain install nightly-2020-09-27
+# ENV PATH="/root/.cargo/bin:${PATH}"
 
-RUN rustup default nightly-2020-09-27
+# RUN rustup toolchain install nightly
 
-RUN rustup target add wasm32-unknown-unknown --toolchain nightly-2020-09-27
+# RUN rustup default nightly
+
+# RUN rustup target add wasm32-unknown-unknown --toolchain nightly
 
 COPY . .
 
-RUN WASM_BUILD_TOOLCHAIN=nightly-2020-09-27 cargo build --release
+RUN cargo build --release
 
-## Only run for benchmarking
+# CMD ./target/release/node-template --dev
+
+# Only run for benchmarking
 # RUN cd bin/node-template/node && WASM_BUILD_TOOLCHAIN=nightly-2020-09-27 cargo build --release --features runtime-benchmarks && cd ../../..
 
 RUN mkdir /aquila-node && mkdir /aquila-node/target && mkdir /aquila-node/target/release
 
 RUN cp -r target/release/node-template /aquila-node/target/release/node-template && cp -r config /aquila-node/config
 
-RUN cp -r target/release/substrate /aquila-node/target/release/substrate
+# RUN cp -r target/release/substrate /aquila-node/target/release/substrate
 
-RUN cp -r target/release/node-bench /aquila-node/target/release/node-bench
+# RUN cp -r target/release/node-bench /aquila-node/target/release/node-bench
 
 RUN rm -r /substrate
 
 FROM substrate as aquila-node
 
-RUN chmod +x /aquila-node/config/docker-run.sh
+# RUN chmod +x /aquila-node/config/docker-run.sh
 
-ENTRYPOINT ["/aquila-node/config/docker-run.sh"]
+# ENTRYPOINT ["/aquila-node/config/docker-run.sh"]
 
-
-
-
-
-
-
+RUN nohup /aquila-node/target/release/node-template \
+  --telemetry-url 'wss://telemetry.polkadot.io/submit/ 0' \
+	--base-path /tmp/validator_1 \
+	--chain=/aquila-node/config/chainSpecRaw.json \
+	--port 30333 \
+  --pruning 256 \
+	--ws-port 9944 \
+	--rpc-port 9933 \
+  --rpc-cors all \
+	--validator \
+  --ws-external \
+  --rpc-external \
+	--rpc-methods=Unsafe \
+  --offchain-worker WhenValidating \
+	--name node_validator &
 
--- config/docker-run.sh
+++ config/docker-run.sh
@@ -1,102 +1,102 @@
 #!/usr/bin/env bash
 
-# ===================== RUN OPERATIONAL TXS BENCHMARKS ==========================
-# DB Read benchmark
-echo Running... DB Read benchmark
-/provenance-node/target/release/node-bench '::trie::read::large' &> ./database-read.txt
+# # ===================== RUN OPERATIONAL TXS BENCHMARKS ==========================
+# # DB Read benchmark
+# echo Running... DB Read benchmark
+# /aquila-node/target/release/node-bench '::trie::read::large' &> ./database-read.txt
 
-# DB Write benchmark
-echo Running... DB Write benchmark
-/provenance-node/target/release/node-bench '::trie::write::large' &> ./database-write.txt
+# # DB Write benchmark
+# echo Running... DB Write benchmark
+# /aquila-node/target/release/node-bench '::trie::write::large' &> ./database-write.txt
 
-# Empty Block Construction
-echo Running... Empty Block Construction
-/provenance-node/target/release/node-bench '::node::import::wasm::sr25519::noop::rocksdb::empty' &> ./empty-block-construction.txt
+# # Empty Block Construction
+# echo Running... Empty Block Construction
+# /aquila-node/target/release/node-bench '::node::import::wasm::sr25519::noop::rocksdb::empty' &> ./empty-block-construction.txt
 
-# Extrinsic Overhead
-echo Running... Extrinsic Overhead
-/provenance-node/target/release/node-bench '::node::import::wasm::sr25519::noop::rocksdb::custom' --transactions 10000 &> ./extrinsic-overhead.txt
+# # Extrinsic Overhead
+# echo Running... Extrinsic Overhead
+# /aquila-node/target/release/node-bench '::node::import::wasm::sr25519::noop::rocksdb::custom' --transactions 10000 &> ./extrinsic-overhead.txt
 
 
-# # ==================== RUN NON-OPERATIONAL TXS BENCHMARKS =======================
+# # # ==================== RUN NON-OPERATIONAL TXS BENCHMARKS =======================
 
-# ----------------------------- PROVENANCE LEDGER ---------------------------------
-# add_activity_group
-echo Running... add_activity_group
-/provenance-node/target/release/substrate benchmark --chain dev \
-    --execution=wasm \
-    --wasm-execution=compiled \
-    --pallet provenance_ledger \
-    --extrinsic add_activity_group \
-    --steps 50 \
-    --repeat 200000 \
-    --raw &> ./provenance_add_activity_group.txt
+# # ----------------------------- PROVENANCE LEDGER ---------------------------------
+# # add_activity_group
+# echo Running... add_activity_group
+# /aquila-node/target/release/substrate benchmark --chain dev \
+#     --execution=wasm \
+#     --wasm-execution=compiled \
+#     --pallet provenance_ledger \
+#     --extrinsic add_activity_group \
+#     --steps 50 \
+#     --repeat 200000 \
+#     --raw &> ./provenance_add_activity_group.txt
 
-# ----------------------------- PALLET BALANCES -----------------------------------
-# transfer
-echo Running... transfer
-/provenance-node/target/release/substrate benchmark --chain dev \
-    --execution=wasm --wasm-execution=compiled \
-    --pallet pallet_balances \
-    --extrinsic transfer \
-    --steps 50 \
-    --repeat 200000 \
-    --raw &> ./balances_transfer.txt
+# # ----------------------------- PALLET BALANCES -----------------------------------
+# # transfer
+# echo Running... transfer
+# /aquila-node/target/release/substrate benchmark --chain dev \
+#     --execution=wasm --wasm-execution=compiled \
+#     --pallet pallet_balances \
+#     --extrinsic transfer \
+#     --steps 50 \
+#     --repeat 200000 \
+#     --raw &> ./balances_transfer.txt
 
-# transfer_keep_alive
-echo Running... transfer_keep_alive
-/provenance-node/target/release/substrate benchmark --chain dev \
-    --execution=wasm --wasm-execution=compiled \
-    --pallet pallet_balances \
-    --extrinsic transfer_keep_alive \
-    --steps 50 \
-    --repeat 200000 \
-    --raw &> ./balances_transfer_keep_alive.txt
+# # transfer_keep_alive
+# echo Running... transfer_keep_alive
+# /aquila-node/target/release/substrate benchmark --chain dev \
+#     --execution=wasm --wasm-execution=compiled \
+#     --pallet pallet_balances \
+#     --extrinsic transfer_keep_alive \
+#     --steps 50 \
+#     --repeat 200000 \
+#     --raw &> ./balances_transfer_keep_alive.txt
 
-# set_balance_creating
-echo Running... set_balance_creating
-/provenance-node/target/release/substrate benchmark --chain dev \
-    --execution=wasm --wasm-execution=compiled \
-    --pallet pallet_balances \
-    --extrinsic set_balance_creating \
-    --steps 50 \
-    --repeat 200000 \
-    --raw &> ./balances_set_balance_creating.txt
+# # set_balance_creating
+# echo Running... set_balance_creating
+# /aquila-node/target/release/substrate benchmark --chain dev \
+#     --execution=wasm --wasm-execution=compiled \
+#     --pallet pallet_balances \
+#     --extrinsic set_balance_creating \
+#     --steps 50 \
+#     --repeat 200000 \
+#     --raw &> ./balances_set_balance_creating.txt
 
-# set_balance_killinge
-echo Running... set_balance_killinge
-/provenance-node/target/release/substrate benchmark --chain dev \
-    --execution=wasm --wasm-execution=compiled \
-    --pallet pallet_balances \
-    --extrinsic set_balance_killing \
-    --steps 50 \
-    --repeat 200000 \
-    --raw &> ./balances_set_balance_killing.txt
+# # set_balance_killinge
+# echo Running... set_balance_killinge
+# /aquila-node/target/release/substrate benchmark --chain dev \
+#     --execution=wasm --wasm-execution=compiled \
+#     --pallet pallet_balances \
+#     --extrinsic set_balance_killing \
+#     --steps 50 \
+#     --repeat 200000 \
+#     --raw &> ./balances_set_balance_killing.txt
 
-# force_transfer
-echo Running... force_transfer
-/provenance-node/target/release/substrate benchmark --chain dev \
-    --execution=wasm --wasm-execution=compiled \
-    --pallet pallet_balances \
-    --extrinsic force_transfer \
-    --steps 50 \
-    --repeat 200000 \
-    --raw &> ./balances_force_transfer.txt
+# # force_transfer
+# echo Running... force_transfer
+# /aquila-node/target/release/substrate benchmark --chain dev \
+#     --execution=wasm --wasm-execution=compiled \
+#     --pallet pallet_balances \
+#     --extrinsic force_transfer \
+#     --steps 50 \
+#     --repeat 200000 \
+#     --raw &> ./balances_force_transfer.txt
 
 
 # ==================== RUN THE NODE======================================
 
-FILE=/provenance-node/config/chainSpecRaw.json
+FILE=/aquila-node/config/customSpecRaw.json
 if test -f "$FILE"; then
     echo "chainSpecRaw.json provided"
 else
-  /provenance-node/target/release/node-template build-spec --chain=/provenance-node/config/chainSpec.json --raw --disable-default-bootnode > /provenance-node/config/chainSpecRaw.json
+  /aquila-node/target/release/node-template build-spec --chain=/aquila-node/config/chainSpec.json --raw --disable-default-bootnode > /aquila-node/config/chainSpecRaw.json
 fi
 
-nohup /provenance-node/target/release/node-template \
+nohup /aquila-node/target/release/node-template \
   --telemetry-url 'wss://telemetry.polkadot.io/submit/ 0' \
 	--base-path /tmp/validator_1 \
-	--chain=/provenance-node/config/chainSpecRaw.json \
+	--chain=/aquila-node/config/chainSpecRaw.json \
 	--port 30333 \
   --pruning 256 \
 	--ws-port 9944 \
@@ -109,32 +109,32 @@ nohup /provenance-node/target/release/node-template \
   --offchain-worker WhenValidating \
 	--name node_validator &
 
-sleep 5
+# sleep 5
 
-curl http://localhost:9933 -H "Content-Type:application/json;charset=utf-8" -d "@/provenance-node/config/aura-keys.json"
+# curl http://localhost:9933 -H "Content-Type:application/json;charset=utf-8" -d "@/aquila-node/config/aura-keys.json"
 
-curl http://localhost:9933 -H "Content-Type:application/json;charset=utf-8" -d "@/provenance-node/config/grandpa-keys.json"
+# curl http://localhost:9933 -H "Content-Type:application/json;charset=utf-8" -d "@/aquila-node/config/grandpa-keys.json"
 
-curl http://localhost:9933 -H "Content-Type:application/json;charset=utf-8" -d "@/provenance-node/config/offchain-worker-fqs!-keys.json"
+# curl http://localhost:9933 -H "Content-Type:application/json;charset=utf-8" -d "@/aquila-node/config/offchain-worker-fqs!-keys.json"
 
-kill $(ps aux | grep '[t]arget/release/node-template' | awk '{print $2}')
+# kill $(ps aux | grep '[t]arget/release/node-template' | awk '{print $2}')
 
-rm -rf ./provenance-node/config/aura-keys.json
+# rm -rf ./aquila-node/config/aura-keys.json
 
-rm -rf ./provenance-node/config/grandpa-keys.json
+# rm -rf ./aquila-node/config/grandpa-keys.json
 
-/provenance-node/target/release/node-template \
-  --telemetry-url 'wss://telemetry.polkadot.io/submit/ 0' \
-	--base-path /tmp/validator_1 \
-	--chain=/provenance-node/config/chainSpecRaw.json \
-	--port 30333 \
-	--ws-port 9944 \
-  --pruning 256 \
-	--rpc-port 9933 \
-  --rpc-cors all \
-	--validator \
-  --ws-external \
-  --rpc-external \
-	--rpc-methods=Unsafe \
-  --offchain-worker WhenValidating /
-	--name node_validator
+# /aquila-node/target/release/node-template \
+#   --telemetry-url 'wss://telemetry.polkadot.io/submit/ 0' \
+# 	--base-path /tmp/validator_1 \
+# 	--chain=/aquila-node/config/chainSpecRaw.json \
+# 	--port 30333 \
+# 	--ws-port 9944 \
+#   --pruning 256 \
+# 	--rpc-port 9933 \
+#   --rpc-cors all \
+# 	--validator \
+#   --ws-external \
+#   --rpc-external \
+# 	--rpc-methods=Unsafe \
+#   --offchain-worker WhenValidating /
+# 	--name node_validator
